7377bc3170e6a0f5df8d1786dc188f04c6105f21
Forhad Rahman <81707858+forhad-61@users.noreply.github.com>
2021-08-04 13:57:52 +0600

    Trying with Int

--- 1. current.cpp
+++ 1. current.cpp
@@ -1,40 +1,21 @@
 #include <iostream>
-#include <string>
 #include <vector>
 
-class User {
-    public:
-    std::string first_name{};
-    std::string last_name{};
-};
-
-int add_new(std::vector<User>, User user) {
-    for()
-    if()
-
+void vec_loop(std::vector<std::string> vec, vec.size()) {
+    for(auto i = 0; i < vecSize; i++) {
+        std::cout << vec[i] << '\n';
+    }
 }
 
 int main() {
 
-    User billy;
-    User kelly;
-    User milly;
+    std::vector<std::string> myString;
+    myString.push_back("New String 1");
+    myString.push_back("New String 2");
+    myString.push_back("New String 3");
 
-    billy.first_name = "Billy";
-    billy.last_name = "Margaret";
+    vec_loop(myString, myString.size());
 
-    kelly.first_name = "Kelly";
-    kelly.last_name = "Abraham";
-
-    milly.first_name = "Milly";
-    milly.last_name = "Jhonson";
-
-    std::vector<User> userVec;
-    userVec.push_back(billy);
-    userVec.push_back(kelly);
-    userVec.push_back(milly);
-
-    add_new(userVec, milly);
 
     return 0;
 }
