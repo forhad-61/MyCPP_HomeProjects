633b18361862775c31b48f42e465f908e6de5971
Forhad Rahman <81707858+forhad-61@users.noreply.github.com>
2021-08-04 13:56:54 +0600

    sizeof all

--- 1. current.cpp
+++ 1. current.cpp
@@ -1,22 +1,40 @@
 #include <iostream>
-#include <string>
-#include <vector>
 
-class User {
-    public:
-    std::string first_name{};
-    std::string last_name{};
-};
-
-
+void printer(int ray[], int size) {
+    std::cout << "In fucntion: ";
+    std::cout << "size: " << sizeof(ray) / sizeof(int);
+    for(int i = 0; i < size; i++) {
+        std::cout << ray[i];
+    }
+    std::cout << '\n';
+    std::cout << '\n';
+}
 
 int main() {
 
-    User billy;
-    billy.first_name = "Billy";
-    billy.last_name = "Margaret";
+    int array[] = {4, 8, 9, 6, 45, 1, 87, 7};
+    int size{8};
 
-    std::cout << billy.first_name;
+
+    std::cout << "Before function: \n";
+    std::cout << "size: " << sizeof(array) / sizeof(int);
+    for(int i = 0; i < size; i++) {
+        std::cout << array[i];
+    }
+    std::cout << '\n';
+    std::cout << '\n';
+
+    printer(array, size); // function call
+
+    std::cout << "After passing in function: \n";
+    std::cout << "size: " << sizeof(array) / sizeof(int);
+    for(int i = 0; i < size; i++) {
+        std::cout << array[i];
+    }
+
+    std::cout << '\n';
+    std::cout << '\n';
+
 
     return 0;
 }
