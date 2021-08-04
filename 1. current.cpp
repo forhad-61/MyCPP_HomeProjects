ad1d77391731d79a7fe39705fce28d1d409ac3e9
Forhad Rahman <81707858+forhad-61@users.noreply.github.com>
2021-08-04 13:56:12 +0600

    Practicing Class Running

--- 1. current.cpp
+++ 1. current.cpp
@@ -1,38 +1,22 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
-void printer(int ray[], int size) {
-    std::cout << "In fucntion: ";
+class User {
+    public:
+    std::string first_name{};
+    std::string last_name{};
+};
 
-    for(int i = 0; i < size; i++) {
-        std::cout << ray[i];
-    }
-    std::cout << '\n';
-    std::cout << '\n';
-}
+
 
 int main() {
 
-    int array[] = {4, 8, 9, 6, 45, 1, 87, 7};
-    int size{8};
+    User billy;
+    billy.first_name = "Billy";
+    billy.last_name = "Margaret";
 
-
-    std::cout << "Before function: \n";
-    for(int i = 0; i < size; i++) {
-        std::cout << array[i];
-    }
-    std::cout << '\n';
-    std::cout << '\n';
-
-    printer(array, size);
-
-    std::cout << "After passing in function: \n";
-    for(int i = 0; i < size; i++) {
-        std::cout << array[i];
-    }
-
-    std::cout << '\n';
-    std::cout << '\n';
-
+    std::cout << billy.first_name;
 
     return 0;
 }
