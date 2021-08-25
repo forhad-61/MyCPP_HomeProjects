a45251d8e2ce77ee5709ead41a72aef5791c675a
Forhad Rahman <81707858+forhad-61@users.noreply.github.com>
2021-08-25 16:49:41 +0600

    Fix operator precedence with braces in function

--- 1. current.cpp
+++ 1. current.cpp
@@ -1,40 +1,16 @@
 #include <iostream>
-#include <string>
-using std::endl;
 
-class Food_maker{
-private:
-    char m_cake_maker;
-    char m_blender;
-    char m_water_filter;
-    int m_number_of_serve;
+void add_two(int x) {
+    std::cout << (x += 2); 
+    // since << has higher precedence than +=, 
+    // if you don't wrap the equation with braces, 
+    // it complains error. 
+}
 
-public:
-    Food_maker(char cake, char blender, char filter, int total) :
-    m_cake_maker{cake},
-    m_blender{blender},
-    m_water_filter{filter},
-    m_number_of_serve{total} {
+int main() {
 
-    }
-
-    int getTotal() {
-        return m_number_of_serve;
-    }
-
-    char getCake() { 
-        // remember to put the data type of the function. 
-        // Otherwise it prints the ascii value of char (67). 
-        // Putting no data type shows no complain from the compiler. 
-        return m_cake_maker;
-    }
-};
-
-int main(){
-
-    Food_maker Burger_home('C', 'B', 'F', 3);
-    std::cout << Burger_home.getTotal() << std::endl;
-    std::cout << Burger_home.getCake();
+    int x{1};
+    add_two(x);
 
     return 0;
-}
+} 
