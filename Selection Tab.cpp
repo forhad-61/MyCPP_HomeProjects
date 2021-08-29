0c78117839a049e69c016066d2205d70c2466682
Forhad Rahman <81707858+forhad-61@users.noreply.github.com>
2021-08-29 23:47:06 +0600

    Move the selection tab in a function

--- Selection Tab.cpp
+++ Selection Tab.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <string>
 
+void selection_tab() {
+    std::cout << "a. play a song lyrics" << std::endl;
+    std::cout << "b. throw a random number" << std::endl;
+    std::cout << "c. enter your name & display it" << std::endl;
+    std::cout << "d. write some number & sort them asc order" << std::endl;
+    std::cout << "x. terminate the program!" << std::endl << std::endl;
+}
+
 int main() {
 
     std::cout << "=================================================" << std::endl;
@@ -8,18 +16,27 @@ int main() {
     std::cout << "=================================================" << std::endl << std::endl;
 
     std::cout << "Selection one option below by using the characters before them or press 'x' to terminate the program at anytime!" << std::endl << std::endl;
-
-    std::cout << "a. play a song lyrics" << std::endl;
-    std::cout << "b. throw a random number" << std::endl;
-    std::cout << "c. enter your name & display it" << std::endl;
-    std::cout << "d. write some number & sort them asc order" << std::endl;
-    std::cout << "x. terminate the program!" << std::endl << std::endl;
+    selection_tab();
 
     char latter;
     std::cin >> latter;
+
     switch(latter) {
 case 'a':
-    std::cout << "\"Close To The Sun\"" << std::endl;
+    std::cout << "\n         \"Close To The Sun\"\n";
+    std::cout << "     ==========================" << std::endl;
+    std::cout << "          Let's sing a song\n"
+              << "      Full of hope, full of pain\n"
+              << " Why don't you sing along, my friend?\n"
+              << "      For it's our last refrain\n"
+              << "          Forever young\n"
+              << "      Ever strong, ever brave\n"
+              << "    Memories like this never end\n"
+              << "      No, they don't fade away\n" << std::endl;
+              break;
+              selection_tab();
+case 'b':
+    std::cout << "now in b";
     }
 
     return 0;
